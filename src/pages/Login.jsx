a8736d39f0ef86e0591bfcf147033e7a2f48e54f
JonaldyBensharon <jonaldybenstar@gmail.com>
2025-12-09 08:47:14 +0700

    Penyesuaian path untuk integrasi back-end yang berubah

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,8 +11,7 @@ export default function Login({ onLogin }) {
     const endpoint = isRegister ? 'register' : 'login';
     
     try {
-        // PENTING: Gunakan Port 5001
-        const res = await fetch(`http://127.0.0.1:5001/${endpoint}`, {
+        const res = await fetch(`http://127.0.0.1:5001/api/auth/${endpoint}`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(formData)
